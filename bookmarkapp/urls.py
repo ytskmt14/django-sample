9f3794dfbc87208c9317afec13893de8a802e2d2
yuta sakamoto <sorigeeeen@gmail.com>
2020-06-04 22:08:36 +0900

    fix urls.py

--- bookmarkapp/urls.py
+++ bookmarkapp/urls.py
@@ -4,15 +4,15 @@ from . import views
 
 app_name = 'bookmarkapp'
 urlpatterns = [
-  path('', views.index, name='index'),
-  path('detail/<int:pk>', views.detail, name='detail'),
-  path('top_create', views.top_create, name='top_create'),
-  path('detail/<int:pk>/detail_create', views.detail_create, name='detail_create'),
-  path('detail/<int:top_pk>/sub_detail_create/<int:detail_pk>', views.sub_detail_create, name='sub_detail_create'),
-  path('top_update/<int:pk>', views.top_update, name='top_update'),
-  path('detail/<int:top_pk>/detail_update/<int:pk>', views.detail_update, name='detail_update'),
-  path('detail/<int:top_pk>/sub_detail_update/<int:pk>', views.sub_detail_update, name='sub_detail_update'),
-  path('top_delete/<int:pk>', views.top_delete, name='top_delete'),
-  path('detail/<int:top_pk>/detail_delete/<int:pk>', views.detail_delete, name='detail_delete'),
-  path('detail/<int:top_pk>/sub_detail_delete/<int:pk>', views.sub_detail_delete, name='sub_detail_delete'),
+  path('travels', views.index, name='index'),
+  path('travels/<int:pk>', views.detail, name='detail'),
+  path('travels/create', views.top_create, name='top_create'),
+  path('travels/<int:pk>/edit', views.top_update, name='top_update'),
+  path('travels/<int:pk>/delete', views.top_delete, name='top_delete'),
+  path('travels/<int:pk>/plans/create', views.detail_create, name='detail_create'),
+  path('travels/<int:top_pk>/plans/<int:detail_pk>/create', views.sub_detail_create, name='sub_detail_create'),
+  path('travels/<int:top_pk>/plans/<int:pk>/edit', views.detail_update, name='detail_update'),
+  path('travels/<int:top_pk>/plans/<int:pk>/delete', views.detail_delete, name='detail_delete'),
+  path('travels/<int:top_pk>/plans/<int:pk>/schedules/edit', views.sub_detail_update, name='sub_detail_update'),
+  path('travels/<int:top_pk>/plans/<int:pk>/schedules/delete', views.sub_detail_delete, name='sub_detail_delete'),
 ]
